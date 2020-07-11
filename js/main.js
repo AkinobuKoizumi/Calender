3c90c4807f63e51bd1eca1a407f3eb9d29b8b149
EC2 Default User <ec2-user@ip-172-31-92-238.ec2.internal>
2020-07-11 13:39:51 +0000

    my calender

---
+++ js/main.js
@@ -0,0 +1,162 @@
+'use strict';
+
+{
+    
+    const today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth();  
+    
+    
+    /*
+     *getCalenderHead
+     **カレンダーの 7× 4の表示区画に入り込んだ、対象月以外の表示について
+     **d で先月の最終日を取得
+     **n で今月の1日が何曜日かを数字で取得
+     **n の前の日から7 × 4 の先頭までの先月の日にちを取得(for文)
+     */
+    function getCalenderHead(){
+        const dates=[];
+        const d = new Date(year, month, 0).getDate();
+        const n = new Date(year, month, 1).getDay();
+        
+        for(let i=0;i < n;i++){
+            dates.unshift({
+                date: d - i,
+                isToday: false,
+                isDisabled: true,
+            });
+        }
+        return dates;
+    }
+    
+
+    
+    function getCalenderBody(){
+        const dates = [];
+        const lastDate = new Date(year, month + 1, 0).getDate();
+        
+        for(let i=1;i<=lastDate;i++){
+            dates.push({
+                date: i,
+                isToday: false,
+                isDisabled: false,
+            });
+        }
+        
+        if(year === today.getFullYear() && month === today.getMonth()){
+            dates[today.getDate() -1].isToday = true;            
+        }
+        
+
+        
+        return dates;
+    }
+    function getCalenderTail(){
+        const dates = [];
+        const lastDay = new Date(year, month + 1,0).getDay();
+        
+        for(let i=1; i < 7-lastDay; i++){
+                dates.push({
+                   date: i,
+                   isToday: false,
+                   isDisabled: true,
+                });
+            
+        }
+        return dates;
+    }
+    
+    /*
+     *翌月もしくは先月がクリックされるたびにcreateCalenderされるため、
+     *選択されていない月を削除する必要がある。
+     * =clearCalender
+     */
+    function clearCalender(){
+        const tbody = document.querySelector('tbody');
+        
+        while (tbody.firstChild){
+            tbody.removeChild(tbody.firstChild);
+        }        
+    }
+    /*
+     *0000/00という表示を作る
+     *文字リテラルで表記
+     *padStart(2,'0') では、2桁にならないまでは、0を表記
+     *htmlのIdを取得しtextContent
+     */
+    function renderTitle(){
+        const title = `${year}/${String(month+1).padStart(2,'0')}`;
+        document.getElementById('title').textContent = title;        
+    }
+    
+    function renderWeeks(){
+        const dates = [
+            ...getCalenderHead(),
+            ...getCalenderBody(),
+            ...getCalenderTail(),
+            ];
+            
+            const weeks = [];
+            const weeksCount = dates.length / 7;
+            
+            for(let i = 0; i < weeksCount; i++){
+                weeks.push(dates.splice(0,7));
+            }
+            
+            weeks.forEach(week => {
+                const tr = document.createElement('tr');
+                week.forEach(date =>{
+                    const td = document.createElement('td');
+                    
+                    td.textContent = date.date;
+                    if(date.isToday){
+                        td.classList.add('today');
+                    }
+                    if(date.isDisabled){
+                        td.classList.add('disabled');
+                    }
+                    tr.appendChild(td);
+                });
+                document.querySelector('tbody').appendChild(tr);
+            });        
+    }
+    
+    
+    function createCalender(){
+        clearCalender();
+        renderTitle();
+        renderWeeks();
+    }
+    
+    
+    /*
+     *イベント系
+     */
+    document.getElementById('prev').addEventListener('click', () => {
+        month--;
+        if(month < 0){
+            year--;
+            month = 11;
+        }
+        createCalender();
+    });
+    
+    document.getElementById('next').addEventListener('click', () => {
+       month++;
+       if(month > 11){
+           year++;
+           month = 0;
+       }
+       createCalender();
+    });
+    
+    document.getElementById('today').addEventListener('click', () => {
+        year = today.getFullYear();
+        month = today.getMonth();
+        
+        createCalender();
+    });
+    
+    createCalender();
+    
+}
